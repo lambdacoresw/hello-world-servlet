0467be3dbc11c56a2146c4b3dcbf346707e9d4dd
Tung Nguyen <tongueroo@gmail.com>
2018-02-05 20:34:10 -0800

    working!

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
 // Extend HttpServlet class
 public class HelloWorld extends HttpServlet {
@@ -16,12 +17,41 @@ public class HelloWorld extends HttpServlet {
    public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
+      Connection c = null;
+      String value = null;
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager
+            .getConnection("jdbc:postgresql://localhost:5432/postgres",
+            "tung", "");
+
+         // ActiveRecord::Base.connection.select_value('SELECT version()')
+         // https://alvinalexander.com/java/java-mysql-select-query-example
+         // our SQL SELECT query.
+         // if you only need a few columns, specify them by name instead of using "*"
+         String query = "SELECT version()";
+         // create the java statement
+         Statement st = c.createStatement();
+         // execute the query, and get a java resultset
+         ResultSet rs = st.executeQuery(query);
+
+         rs.next();
+         value = rs.getString(1);
+         System.out.println("value: " + value);
+
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+      System.out.println("Opened database successfully");
+
       // Set response content type
       response.setContentType("text/html");
 
       // Actual logic goes here.
       PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
+      out.println("<h1>" + value + "</h1>");
    }
 
    public void destroy() {
--- README.md
+++ README.md
@@ -6,3 +6,10 @@ mv HelloWorld.class /usr/local/opt/tomcat/libexec/webapps/ROOT/WEB-INF/classes/H
 cp /usr/local/opt/tomcat/libexec/webapps/ROOT/WEB-INF/web.xml{,.bak}
 cp web.xml /usr/local/opt/tomcat/libexec/webapps/ROOT/WEB-INF/web.xml
 ```
+
+
+export CLASSPATH=/Users/tung/src/java/lib/postgresql-42.2.1.jar:/usr/local/Cellar/tomcat/8.5.27/libexec/lib/servlet-api.jar
+
+
+mkdir -p /usr/local/opt/tomcat/libexec/webapps/ROOT/WEB-INF/lib/
+cp /Users/tung/src/java/lib/postgresql-42.2.1.jar /usr/local/opt/tomcat/libexec/webapps/ROOT/WEB-INF/lib/
